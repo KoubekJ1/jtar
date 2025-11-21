6531b5257eb5f4e8ffd32c89609060a7eea1273d
Jan Koubek <koubek.jenda@icloud.com>
2025-11-21 16:16:11 +0100

    Implemented log debug option

--- Jtar/Logger/Logger.cs
+++ Jtar/Logger/Logger.cs
@@ -5,13 +5,35 @@ namespace Jtar.Logging;
 
 public static class Logger
 {
+    private static readonly object _lock = new object();
+    private static bool _showDebugMessages = false;
+
+    public static bool ShowDebugMessages
+    {
+        get
+        {
+            lock (_lock)
+            {
+                return _showDebugMessages;
+            }
+        }
+        set
+        {
+            lock (_lock)
+            {
+                _showDebugMessages = value;
+            }
+        }
+    }
+
     public static void Log(LogType type, string message)
     {
-
-        Console.WriteLine(
-            $"[{DateTime.Now:HH:mm:ss.fff}] [{type.ToString().ToUpper()}] {message}"
-        );
-
+        if (type == LogType.Debug && !ShowDebugMessages)
+        {
+            return;
+        }
+        var prefix = ShowDebugMessages ? $"[{DateTime.Now:HH:mm:ss.fff}] [{type.ToString().ToUpper()}] " : "";
+        Console.WriteLine($"{prefix}{message}");
     }
 
     public static void Log(Exception ex)
